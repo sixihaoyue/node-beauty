5b16ad586d72f3429d647cd8650f785153537344
dianshangyihaof <dianshangyihaof@foxmail.com>
2016-08-16 18:00:24 +0800

    add getCore function

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 "use strict";
 let options = {}, services = {}, server = null;
+let getServer = (next) => {
+    if (!server) {
+      options.config = options.config || 'etc/config.js';
+      options.path = options.path || 'sys';
+      server = require('./lib/core.js')(options, services);
+    }
+    return server;
+};
 module.exports = {
   init: (o) => {
     options = o;
   },
-  start: (next) => {
-    options.config = options.config || 'etc/config.js';
-    options.path = options.path || 'sys';
-    server = require('./lib/core.js')(options, services, next);
-  },
+  getCore: () => getServer().getCore(),
+  start: (next, onExit) => getServer().start(next, onExit),
   use: (serviceName, instance) => services[serviceName] = instance || null
 };
